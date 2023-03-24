80f608fa8cf501d7c3eaa59b233ea6c106710fea
dusdjhyeon <dusdj0813@gmail.com>
2023-03-24 21:09:29 +0900

    modifying variables.tf

--- variables.tf
+++ variables.tf
@@ -2,7 +2,19 @@ variable "project_id" {
   type = string
 }
 
+variable "env_name" {
+  type = string
+}
+
 variable "region" {
+  type = string
+}
+
+variable "vpc_name" {
+  type = string
+}
+
+variable "cluster_name" {
   type = string
 }
 
